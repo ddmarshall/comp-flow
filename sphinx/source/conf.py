e093f933a5f00ba3fa7ce362bee8701b36feeaf6
ddmarshall <ddmarshall@gmail.com>
2023-06-22 16:43:00 -0700

    Added skeleton sphinx documentation.

---
+++ sphinx/source/conf.py
@@ -0,0 +1,65 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# pylint: skip-file
+
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'comp-flow'
+copyright = '2023, David D. Marshall'
+author = 'David D. Marshall'
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary'
+]
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+autosummary_generate = True
+html_show_sourcelink = False
+autodoc_inherit_docstrings = True
+autodoc_member_order = 'bysource'
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+
+# from readthedocs
+html_theme_options = {
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    # 'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
---
+++ sphinx/source/index.rst
@@ -0,0 +1,29 @@
+Welcome to comp-flow's documentation!
+==========================================
+
+**comp-flow** is a Python library for modeling a wide variety of compressible flows using thermodynamics and gas dynamics relations.
+
+.. note::
+
+   This project is under development and the API might change between releases.
+
+.. include:: ../../README.rst
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Home <self>
+   Examples <examples/index.rst>
+   API <api/modules.rst>
+   Developer Resources <developers/index.rst>
+   Project <https://github.com/ddmarshall/comp-flow>
+   Documentation <https://ddmarshall.github.io/comp-flow>
+   License <license/index.rst>
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
